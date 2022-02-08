8010ca93d9e651fea366a03d671abef47f0fa68c
Thorsten Lorenz <thlorenz@gmx.de>
2022-02-08 14:36:51 -0600

    init-vault: tests for different args and error case

--- token-vault/js/test/init-vault.ts
+++ token-vault/js/test/init-vault.ts
@@ -4,6 +4,7 @@ import { addressLabels, killStuckProcess, spokSameBignum, spokSamePubkey } from 
 import {
   airdrop,
   assertConfirmedTransaction,
+  assertError,
   assertTransactionSummary,
   PayerTransactionHandler,
 } from '@metaplex-foundation/amman';
@@ -118,4 +119,78 @@ test('init-vault: init vault allowing further share creation', async (t) => {
     state: VaultState.Inactive,
     lockedPricePerShare: spokSameBignum(0),
   });
+});
+
+test('init-vault: init vault not allowing further share creation', async (t) => {
+  const { transactionHandler, connection, authority } = await init();
+  const initVaultAccounts = await initInitVaultAccounts(
+    t,
+    connection,
+    transactionHandler,
+    authority,
+  );
+
+  const initVaultIx = await InitVault.initVault(initVaultAccounts, {
+    allowFurtherShareCreation: false,
+  });
+
+  const initVaulTx = new Transaction().add(initVaultIx);
+  const initVaultRes = await transactionHandler.sendAndConfirmTransaction(initVaulTx, []);
+
+  assertConfirmedTransaction(t, initVaultRes.txConfirmed);
+  assertTransactionSummary(t, initVaultRes.txSummary, {
+    msgRx: [/Init Vault/, /success/],
+  });
+
+  const { fractionMint, redeemTreasury, fractionTreasury, vault, pricingLookupAddress } =
+    initVaultAccounts;
+
+  const vaultAccountInfo = await connection.getAccountInfo(vault);
+  const [vaultAccount] = Vault.fromAccountInfo(vaultAccountInfo);
+
+  spok(t, vaultAccount, {
+    $topic: 'vaultAccount',
+    key: Key.VaultV1,
+    tokenProgram: spokSamePubkey(TOKEN_PROGRAM_ID),
+    fractionMint: spokSamePubkey(fractionMint),
+    redeemTreasury: spokSamePubkey(redeemTreasury),
+    fractionTreasury: spokSamePubkey(fractionTreasury),
+    pricingLookupAddress: spokSamePubkey(pricingLookupAddress),
+    authority: spokSamePubkey(authority),
+    allowFurtherShareCreation: false,
+    tokenTypeCount: 0,
+    state: VaultState.Inactive,
+    lockedPricePerShare: spokSameBignum(0),
+  });
+});
+
+test('init-vault: init vault twice for same account', async (t) => {
+  const { transactionHandler, connection, authority } = await init();
+  const initVaultAccounts = await initInitVaultAccounts(
+    t,
+    connection,
+    transactionHandler,
+    authority,
+  );
+
+  {
+    const initVaultIx = await InitVault.initVault(initVaultAccounts, {
+      allowFurtherShareCreation: true,
+    });
+
+    const initVaulTx = new Transaction().add(initVaultIx);
+    await transactionHandler.sendAndConfirmTransaction(initVaulTx, []);
+  }
+  {
+    const initVaultIx = await InitVault.initVault(initVaultAccounts, {
+      allowFurtherShareCreation: true,
+    });
+
+    const initVaulTx = new Transaction().add(initVaultIx);
+    try {
+      await transactionHandler.sendAndConfirmTransaction(initVaulTx, []);
+    } catch (err: any) {
+      assertError(t, err, [/Init Vault/i, /Already initialized/i]);
+    }
+  }
 });
